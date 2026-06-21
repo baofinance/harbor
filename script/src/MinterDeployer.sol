6ae7d94884898ce7faef0415e2e4a03aa31f251e
rootminus0x1 <rootminus0x1@gmail.com>
2026-06-21 20:24:41 +0100

    consistent naming for external addresses

--- script/src/MinterDeployer.sol
+++ script/src/MinterDeployer.sol
@@ -230,14 +230,14 @@ abstract contract MinterDeployer is PeggedToken, LeveragedToken, Minter, Stabili
     /// @notice Resolve the wrapped price oracle address for a market.
     /// @dev Default: predicts the CREATE3 address of the pre-deployed oracle from harbor-price-aggregators.
     ///      Override in test setups to deploy a mock oracle instead.
-    function _resolveWrappedPriceOracle(string memory key) internal virtual returns (address) {
+    function _wrappedPriceOracleAddress(string memory key) internal virtual returns (address) {
         return _predictAddress(key);
     }
 
     function _configureMinter(Config_MinterMarket market, string memory marketKey) internal {
         IHarborConfig cfg = IHarborConfig(address(market));
         address minter = _predictAddress(_key(marketKey, "minter"));
-        address priceOracle = _resolveWrappedPriceOracle(MinterMarketConfigLib.priceOracleKey(market));
+        address priceOracle = _wrappedPriceOracleAddress(MinterMarketConfigLib.priceOracleKey(market));
 
         // Update minter configuration (incentive ratios)
         IMinter(minter).updateConfig(cfg.minterConfig());
